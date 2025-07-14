b1b3cd9aa1a2fe623b3f464a4b2c5a06bf94eab6
Riya Maurya <146939873+Riyamaurya123@users.noreply.github.com>
2025-07-14 17:17:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOption = {
-    origin:"https://job-portal-frontend-two-delta.vercel.app/",
+    origin:"https://job-portal-frontend-two-delta.vercel.app",
     credentials:true
 }
 app.use(cors(corsOption))
