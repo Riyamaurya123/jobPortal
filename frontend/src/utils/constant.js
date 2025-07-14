d13533e9dc1c1d031839c75a273bcb76416f3412
Riya Maurya <146939873+Riyamaurya123@users.noreply.github.com>
2025-07-14 19:35:57 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://job-portal-0nuu.onrender.com/api/user"
-export const JOB_API_END_POINT="https://job-portal-0nuu.onrender.com/api/job"
-export const APPLICATION_API_END_POINT="https://job-portal-0nuu.onrender.com/api/application"
-export const COMPANY_API_END_POINT="https://job-portal-0nuu.onrender.com/api/company"
+export const USER_API_END_POINT="https://jobportal-backend-ggbf.onrender.com/api/user"
+export const JOB_API_END_POINT="https://jobportal-backend-ggbf.onrender.com/api/job"
+export const APPLICATION_API_END_POINT="https://jobportal-backend-ggbf.onrender.com/api/application"
+export const COMPANY_API_END_POINT="https://jobportal-backend-ggbf.onrender.com/api/company"
