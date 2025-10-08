3cf5325681a1dc9c3839e8617e2fd2e66a56c9b9
Riya Maurya <mauryariya234@gmail.com>
2025-10-08 16:33:00 +0530

    update

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 export const USER_API_END_POINT = "http://localhost:6005/api/user";
 export const JOB_API_END_POINT = "http://localhost:6005/api/job";
 export const APPLICATION_API_END_POINT =
   "http://localhost:6005/api/application";
 export const COMPANY_API_END_POINT = "http://localhost:6005/api/company";
-=======
-export const USER_API_END_POINT="https://jobportal-backend-ggbf.onrender.com/api/user"
-export const JOB_API_END_POINT="https://jobportal-backend-ggbf.onrender.com/api/job"
-export const APPLICATION_API_END_POINT="https://jobportal-backend-ggbf.onrender.com/api/application"
-export const COMPANY_API_END_POINT="https://jobportal-backend-ggbf.onrender.com/api/company"
->>>>>>> 7c015eee18cb0c0c1a635f1eceaa89ea04f26414
